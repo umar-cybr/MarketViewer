8ebbd76b4d6dbc89c3f40ee0f49bd1dcf5a1d2c3
umar-cybr <ghost@1245-X360G11.ltiit.local>
2025-04-05 01:01:27 -0400

    syntax tweak

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 
-aapl = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&outputsize=full&apikey=AKIBVM0JT94E6ST7'
+aapl = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&outputsize=full&apikey=AKIBVM0JT94E6ST7' #api call for apple data
 
-ibm = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=AKIBVM0JT94E6ST7'
+ibm = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=AKIBVM0JT94E6ST7' #api call for ibm data (30 days)
 
 aapl_out = requests.get(aapl)
 aapl_data = aapl_out.json()
@@ -10,7 +10,8 @@ aapl_data = aapl_out.json()
 ibm_out = requests.get(ibm)
 ibm_data = ibm_out.json()
 
-
 print(aapl_data)
 print("--------------------------------------------")
 print(ibm_data)
+
+
